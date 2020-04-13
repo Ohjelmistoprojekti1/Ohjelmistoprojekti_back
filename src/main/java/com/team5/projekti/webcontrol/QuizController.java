de53d3e97b918eb416a5986c281825af756b5d6f
EllaRaveala <ella-katarina@hotmail.com>
2020-04-13 13:45:03 +0300

    thymeleaf templatet ja controller luokan päivitys

--- src/main/java/com/team5/projekti/webcontrol/QuizController.java
+++ src/main/java/com/team5/projekti/webcontrol/QuizController.java
@@ -3,15 +3,18 @@ package com.team5.projekti.webcontrol;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.team5.projekti.domain.Radio;
 import com.team5.projekti.domain.RadioRepository;
 
-@RestController
+@Controller
 public class QuizController {
 	
 	@Autowired
@@ -19,8 +22,20 @@ public class QuizController {
 	
 	// Rest-rajapinta yksittäiselle Radio-kysymykselle
 	@RequestMapping(value="/radio/{id}", method=RequestMethod.GET)
-	public Optional<Radio> findRadioQuestion(@PathVariable Long id) {
+	public @ResponseBody Optional<Radio> findRadioQuestion(@PathVariable Long id) {
 		return rqRepository.findById(id);
+	}
+	
+	@RequestMapping(value="/test/radioquestions", method=RequestMethod.GET)
+	public String findAllRadioQuestionsTEST(Model model) {
+		model.addAttribute("radioQuestions", rqRepository.findAll());
+		return "allQuestions";
+	}
+	
+	@RequestMapping(value="/test/radioquestion/{id}", method=RequestMethod.GET)
+	public String findRadioQuestionTEST(@PathVariable Long id, Model model) {
+		model.addAttribute("radioQuestion", rqRepository.findById(id));
+		return "question";
 	}
 	
 }
