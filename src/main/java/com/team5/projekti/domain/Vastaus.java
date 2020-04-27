3a84dbddb8fc10f119068d4e93ca1bf7f5bbfb69
EllaRaveala <54430180+EllaRaveala@users.noreply.github.com>
2020-04-27 20:36:02 +0300

    Vastaus luokka

---
+++ src/main/java/com/team5/projekti/domain/Vastaus.java
@@ -0,0 +1,63 @@
+package com.team5.projekti.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Vastaus {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String answer;
+	
+	@ManyToOne
+	@JoinColumn(name = "id")
+	private Radio question;
+	
+	public Vastaus() {
+		this.answer = null;
+		this.question= null;
+	}
+	
+	public Vastaus(String answer, Radio question) {
+		super();
+		this.answer = answer;
+		this.question= question;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public Kysymys getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Kysymys question) {
+		this.question = question;
+	}
+
+	@Override
+	public String toString() {
+		return "Vastaus [id=" + id + ", answer=" + answer + ", question=" + question + "]";
+	}
+	
+	
+}
