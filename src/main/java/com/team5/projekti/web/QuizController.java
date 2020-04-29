4dc997a388df90b5c4a3b3c601f57ccca1e1cd43
EllaRaveala <54430180+EllaRaveala@users.noreply.github.com>
2020-04-29 15:37:33 +0300

    save vastaus metodi

--- src/main/java/com/team5/projekti/web/QuizController.java
+++ src/main/java/com/team5/projekti/web/QuizController.java
@@ -46,14 +46,20 @@ public class QuizController {
     }
 	
 	// New radio question
-	//@PreAuthorize("hasAuthority('ADMIN')")
+	@PreAuthorize("hasAuthority('ADMIN')")
     	@RequestMapping(value = "/kysymys", method = RequestMethod.POST)
     	 Kysymys newKysymys(@RequestBody Kysymys newKysymys){
         	return rqRepository.save(newKysymys);
-    	 }    
+    	 } 
+	
+	// save answer
+    	@RequestMapping(value = "/vastaus", method = RequestMethod.POST)
+    	 Vastaus saveVastaus(@RequestBody Vastaus vastaus){
+        	return aRepository.save(vastaus);
+    	 } 
     	
     	// Update radio
-	//@PreAuthorize("hasAuthority('ADMIN')")
+	@PreAuthorize("hasAuthority('ADMIN')")
     	@RequestMapping(value = "/kysymys/{id}", method = RequestMethod.PUT)
     	  Kysymys replaceKysymys(@RequestBody Kysymys newKysymys, @PathVariable Long id) {
 
@@ -69,7 +75,7 @@ public class QuizController {
     	  }
     	
    	// Delete radio question
-    	 //@PreAuthorize("hasAuthority('ADMIN')")
+    	 @PreAuthorize("hasAuthority('ADMIN')")
     	 @RequestMapping(value = "/kysymys/{id}", method = RequestMethod.DELETE)
     	 void deleteKysymys(@PathVariable("id") Long Id) {
     		rqRepository.deleteById(Id);
@@ -78,7 +84,7 @@ public class QuizController {
 	
 	//Rest-rajapinta kaikille vastauksille
 	//Vain Admin
-	//@PreAuthorize("hasAuthority('ADMIN')")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value="/answers", method = RequestMethod.GET)
 	public @ResponseBody List<Vastaus> findAllAnswers() {	
 	     return (List<Vastaus>) aRepository.findAll();
