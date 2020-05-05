b4e0a50a1049252e960b8d466e0fca5f87823e54
EllaRaveala <54430180+EllaRaveala@users.noreply.github.com>
2020-05-05 13:23:14 +0300

    sisältö

--- src/main/java/com/team5/projekti/domain/Vastaaja.java
+++ src/main/java/com/team5/projekti/domain/Vastaaja.java
@@ -1,1 +1,48 @@
+package com.team5.projekti.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Vastaaja {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	/*@OneToMany(cascade = CascadeType.ALL, mappedBy = "")
+	private List<Vastaus> answers;*/
+	
+	public Kysymys() {
+		this.question = null;
+	}
+	
+	public Kysymys() {
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	/*public List<Vastaus> getAnswers() {
+		return answers;
+	}
+	public void setAnswers(List<Vastaus> answers) {
+		this.answers = answers;
+	}*/
+	
+	@Override
+	public String toString() {
+		return "Kysymys [id=" + id + "]";
+	}
+
+}
