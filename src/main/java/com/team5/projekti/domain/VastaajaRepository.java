89ae9bcba83b22f5a443c323e29dee7a8b8640b2
Juho Toivonen <juho.toivonen@myy.haaga-helia.fi>
2020-05-05 15:05:03 +0300

    testidataa Vastaaja- ja Vastaus-tauluihin

--- src/main/java/com/team5/projekti/domain/VastaajaRepository.java
+++ src/main/java/com/team5/projekti/domain/VastaajaRepository.java
@@ -3,6 +3,8 @@ package com.team5.projekti.domain;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VastaajaRepository extends CrudRepository<Vastaus, Long> {
+public interface VastaajaRepository extends CrudRepository<Vastaaja, Long> {
     
+	List<Vastaaja> findByEmail(String email);
+	
 }
